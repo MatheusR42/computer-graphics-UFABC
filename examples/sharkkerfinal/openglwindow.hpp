e150d77d49bba783ff2f86df817a8f82f62d6df3
MatheusR42 <matheus.araujo.r42@gmail.com>
2021-12-05 19:12:30 -0300

    init sharkkerfinal

---
+++ examples/sharkkerfinal/openglwindow.hpp
@@ -0,0 +1,74 @@
+#ifndef OPENGLWINDOW_HPP_
+#define OPENGLWINDOW_HPP_
+
+#include <imgui.h>
+
+#include <random>
+
+#include "abcg.hpp"
+#include "model.hpp"
+#include "shark.hpp"
+#include "gamedata.hpp"
+
+class OpenGLWindow : public abcg::OpenGLWindow {
+ protected:
+  void handleEvent(SDL_Event& event) override;
+  void initializeGL() override;
+  void paintGL() override;
+  void paintUI() override;
+  void resizeGL(int width, int height) override;
+  void terminateGL() override;
+  void checkCollisions();
+
+  abcg::ElapsedTimer m_Timer;
+
+  Model m_corals;
+  Shark m_shark;
+
+ private:
+  static const int m_numBubbles{200};
+
+private:
+  static const int m_numCorals{10};
+
+  GLuint m_program{};
+
+  int m_viewportWidth{};
+  int m_viewportHeight{};
+
+  std::default_random_engine m_randomEngine;
+
+  Model m_modelBubble;
+  Model m_modelShark;
+  Model m_modelCoral;
+
+  std::array<glm::vec3, m_numBubbles> m_bubblePositions;
+  std::array<glm::vec3, m_numBubbles> m_bubbleRotations;
+  
+  GameData m_gameData;
+
+  std::array<glm::vec3, m_numCorals> m_coralPositions;
+  std::array<glm::vec3, m_numCorals> m_coralRotations;
+
+  float m_angle{};
+
+  glm::mat4 m_viewMatrix{1.0f};
+  glm::mat4 m_projMatrix{1.0f};
+  float m_FOV{8.0f};
+
+  ImFont* m_font_final{};
+  ImFont* m_font_points{};
+
+
+  // shark
+  abcg::ElapsedTimer m_movimentCoolDownTimer;
+  abcg::ElapsedTimer m_movimentTimer;
+  
+  void restart();
+
+  void randomizeBubble(glm::vec3 &position, glm::vec3 &rotation);
+  void randomizeCoral(glm::vec3 &position, glm::vec3 &rotation);
+  void update();
+};
+
+#endif
---
+++ examples/sharkkerfinal/shark.cpp
@@ -0,0 +1,85 @@
+#include "shark.hpp"
+
+void Shark::setDamage() {
+  m_nodamage = true;                                      // Shark enters on invincible state when hit
+}
+
+void Shark::update(const GameData &gameData) {
+  // auto grey = glm::vec4{0.6f, 0.6f, 0.6f, 1.0f};          // R153 G153 B153 A255
+  // auto damageColor = glm::vec4{1.0f, 0.0f, 0.0f, 1.0f};   // R255 G000 B000 A255
+
+  // if (m_nodamage) {
+  //   m_color = damageColor;                                // Shark turns Red when hit
+  // } else {
+  //   m_color = grey;                                       // Shark turns Grey when hit
+  // }
+  
+
+  // if (m_shakeCoolDownTimer.elapsed() < 20.0/1000.0 || m_angleZ ){
+  //   m_angleZ = m_angleZ + 2;
+  // } else if (m_shakeCoolDownTimer.elapsed() < 40.0/1000.0) {
+  //   m_angleZ = m_angleZ - 2;
+  // } else {
+  //   m_shakeCoolDownTimer.restart();
+  // }
+
+
+  if (m_movimentCoolDownTimer.elapsed() < 35.0/1000.0){    // Shark's axial velocity
+    return;
+  }
+  m_movimentCoolDownTimer.restart();
+
+  // Input Left key
+  if (gameData.m_input[static_cast<size_t>(Input::Left)]) {
+    if (m_positionX > -.18f) {
+      m_positionX = m_positionX - .02;            // Left key sensibility
+      m_angleY = 30.0f;
+    }
+  }
+
+  // Input Right key
+  if (gameData.m_input[static_cast<size_t>(Input::Right)]) {
+    if (m_positionX < .18f) {
+      m_positionX = m_positionX + .02;            // Right key sensibility
+      m_angleY = -30.0f;
+    }
+  }
+
+  // Input Up key
+  if (gameData.m_input[static_cast<size_t>(Input::Up)]) {
+    if (m_positionY < .18f) {
+      m_positionY = m_positionY + .02;            // Up key sensibility
+      m_angleX = 26.0f;
+    }
+  }
+
+  // Input Down key
+  if (gameData.m_input[static_cast<size_t>(Input::Down)]) {
+    if (m_positionY > -.18f) {
+      m_positionY = m_positionY - .02;            // Down key sensibility
+      m_angleX = -26.0f;
+    }
+  }
+
+  if (!gameData.m_input[static_cast<size_t>(Input::Up)] && !gameData.m_input[static_cast<size_t>(Input::Down)]) {
+      m_angleX = 0.0f;
+  }
+
+  if (!gameData.m_input[static_cast<size_t>(Input::Left)] && !gameData.m_input[static_cast<size_t>(Input::Right)]) {
+      if (m_angleY <= -30.0f || m_angleY >= 30.0f) {
+        m_angleY = 0.0f;
+      }
+
+      if (m_angleY < m_angleYBase - 10.0f) {
+        m_shakeDirection = 'R';
+      } else if (m_angleY > m_angleYBase + 10.0f){
+        m_shakeDirection = 'L';
+      }
+
+      if (m_shakeDirection == 'R') {
+        m_angleY = m_angleY + 2;
+      } else {
+        m_angleY = m_angleY - 2;
+      }
+  }
+}
