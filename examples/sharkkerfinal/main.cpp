f9870710c3fa4dedcb0800d19aa4e3d465f8a0c3
MatheusR42 <matheus.araujo.r42@gmail.com>
2021-12-05 21:50:53 -0300

    update game name

--- examples/sharkkerfinal/main.cpp
+++ examples/sharkkerfinal/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv) {
                                .height = 600,
                                .showFPS = false,
                                .showFullscreenButton = false,
-                               .title = "Sharkker 3D"});
+                               .title = "Sharkker Final"});
     app.run(std::move(window));
   } catch (const abcg::Exception &exception) {
     fmt::print(stderr, "{}\n", exception.what());
